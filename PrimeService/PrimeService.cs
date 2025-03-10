675fc4b7048a55dfb2d85596625794952cdf97b0
VIBHASH CHANDRA <vchandra@microsoft.com>
2025-03-10 20:23:51 +0530

    first commit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            
+            int newVar = 1;
+            int otherVar = 2;
+            int mathVar = newVar * otherVar;
+            
+            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
